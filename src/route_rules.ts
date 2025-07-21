d48a4f2c93a5ea56b928edc0487b9c27a4b03dda
CAB233 <yidaduizuoye@outlook.com>
2025-07-21 22:13:02 +0800

    improvement: Restrictions on direct domain

--- src/route_rules.ts
+++ src/route_rules.ts
@@ -43,13 +43,26 @@ export const rejectDomainNoDrop = createRule({
 });
 
 export const directDomain = createRule({
-    rule_set: [
-        rule_set.domainAppleCDN.tag,
-        rule_set.domainMicrosoftCDN.tag,
-        rule_set.domainGameDownload.tag,
-        rule_set.domainAppleCN.tag,
-        rule_set.domainDomestic.tag,
-        rule_set.domainDirect.tag,
+    type: 'logical',
+    mode: 'and',
+    rules: [
+        {
+            rule_set: [
+                rule_set.domainAppleCDN.tag,
+                rule_set.domainMicrosoftCDN.tag,
+                rule_set.domainGameDownload.tag,
+                rule_set.domainAppleCN.tag,
+                rule_set.domainDomestic.tag,
+                rule_set.domainDirect.tag,
+            ],
+        },
+        {
+            rule_set: [
+                rule_set.ipDomestic.tag,
+                rule_set.ipChinaIP.tag,
+                rule_set.ipChinaIPIPv6.tag,
+            ],
+        },
     ],
     outbound: outbounds.direct.tag,
 });
